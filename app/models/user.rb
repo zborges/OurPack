da8284e47936a4f26d973dde3564a4031b7ac062
Zack Borges <zackborges@Zacks-MacBook-Pro.local>
2026-04-18 16:13:21 -0500

    Update gems and fix migration issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_one :pack
   validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 8}, if: -> {password.present}
+  validates :password, length: { minimum: 8}, if: -> {password.present?}
 
 
 end
